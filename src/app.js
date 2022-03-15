6b015874fb1b27de8f4d23bcea32df16b91d101f
John Paul Lopez <jpblopez05@gmail.com>
2022-03-15 22:37:36 +0800

    Adding server

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use((req, res, next) => {
+    console.log(Object.keys(req))
+    next()
+})
 
-app.post('/', (req, res) => {
+app.post('/auth/login', (req, res) => {
+    return res.sendStatus(200)
+
+    // Utils.setCookie('trainToken', res.data.idToken.jwtToken, 1);
+    // Utils.setCookie('trainCurrentPayload', JSON.stringify(res.data.idToken.payload), 1);
+    // Utils.setCookie('trainCurrentUser', res.data.idToken.payload
 
 })
 
